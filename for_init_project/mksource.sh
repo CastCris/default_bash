8e0c145bbd3d9fac85bc33ab03aad226777f0a16
Cristiano <cristianochincarosanches@gmail.com>
2025-05-01 15:08:31 -0300

    update_put_src_function_and_clean_code

--- for_init_project/mksource.sh
+++ for_init_project/mksource.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+# mksource.sh
 
-mksrc(){ # values index
-	local values=("$@")
-	local index=0
-	echo "# sources.sh" > sources.sh
+make_src_options(){ # value #options
+	local values=($1)
+	local options=($2)
+	local arguments=""
+	for i in ${!options[@]};do
+		arguments="$arguments${options[$i]}=${values[$i]} "
+	done
+	echo $arguments
+}
+import_files_to_src(){
 	echo 'source $(find -type f -name import.sh)' >> sources.sh
-
 	local file_sh_imported=($(echo "$PATH_IMPORT_SH/*.sh"))
 	for i in ${file_sh_imported[@]};do
 		if [[ ${i##*/} = "import.sh" ]] || [[ ${i##*/} = "import_src.sh" ]];then
@@ -15,7 +20,16 @@ mksrc(){ # values index
 		echo "$relative_path_file" >> sources.sh
 	done  
 	echo "">>sources.sh
+}
 
+mksrc(){ # values options
+	local values=($1)
+	local options=($2)
+
+	echo -e '# sources.sh\nSOURCES__IMPORTED__="1"\n' > sources.sh
+	import_files_to_src
+
+	local index=0
 	while IFS= read -r line;do
 		local format_line="$line"
 		local temp=${line##*=}
@@ -24,11 +38,13 @@ mksrc(){ # values index
 			format_line="$line\"${values[$index]}\""
 			index=$(($index+1))
 		fi
-		if [[ $OUTPUT = 0 ]];then
-			echo -e "\e[34m$format_line\e[0m"
-		fi
 		echo "$format_line" >> sources.sh
 	done < $(find . -type f -name source_model.sh)
+
+	local arguments="# Options used for construct of project.Don't do anything unless you know what is doing\n $(make_src_options "$(printf "%s " ${values[@]} )" "$(printf "%s " ${options[@]} )")"
+	echo -e $arguments > ${PATH_REPAIR}/arguments.txt
+	
+	echo -e "\e[34m$(cat $(find -type f -name "sources.sh"))\e[0m"
 }
 
 put_path_src(){
@@ -42,18 +58,18 @@ put_path_src(){
 			continue;
 		fi
 
-		local relative_path_temp=$(relative_path "-end=sources.sh -init=${file_sh[0]##*/} -dir_end=$PATH_SRC_SH -dir_start=${file_sh[0]}")
-		relative_path_temp="${relative_path_temp}sources.sh"
-		# echo $relative_path_temp
+		local relative_path_src=$(relative_path "-end=sources.sh -init=${file_sh[0]##*/} -dir_end=$PATH_SRC_SH -dir_start=${file_sh[0]}")
+		relative_path_src_temp="${relative_path_src}sources.sh"
+		echo $relative_path_src_temp
 
-		local relative_path=""
-		for i in `echo $relative_path_temp | tr "/" "\n"`;do
-			relative_path=${relative_path}'\/'$i
+		local relative_path_src=""
+		for i in `echo $relative_path_src_temp | tr "/" "\n"`;do
+			relative_path_src=${relative_path_src}'\/'$i
 		done
-		relative_path=${relative_path:2:${#relative_path}}
-		# echo $relative_path
+		relative_path_src=${relative_path_src:2:${#relative_path_src}}
+		echo $relative_path_src
 		for j in ${file_sh[@]};do
-			sed -i '2s/^/source '$relative_path'\n/' $j
+			sed -i '2s/^/# File signature, dont do anything!\nif [ -z ${SOURCES__IMPORTED__} ];then\n\tPATH_RELATIVE_SH="'${relative_path_src%/*}'"\n\tcd $PATH_RELATIVE_SH\n\tsource "sources.sh"\nfi\n/' $j
 		done
 	done
 }
--- init_project.sh
+++ init_project.sh
@@ -6,6 +6,57 @@ import_file "message_logs.sh"
 import_file "path_files.sh"
 import_file "mksource.sh"
 
+# Global variables
+PROJECT_NAME="my_project"
+PROJECT_PATH="."
+
+# Name directories for main dirs project
+DEVELOP_DIR_NAME="develop"
+SOURCES_DIR_NAME="sources"
+FILE_MAIN_DIR_NAME="is_main"
+# for sh aplications
+SRC_SH="for_bash"
+IMPORT_SH="import_shell"
+# for repair the project
+REPAIR_DIR="maintenance"
+
+# for programming
+SRC_SCRIPT_DIR="src_scripts"
+SRC_MODULE_DIR="src_modules"
+SRC_OBJECT_DIR="src_objects"
+# for C language
+C_BUILD_DIR="c_build"
+C_COMPILE="gcc"
+C_FLAGS="-Wall -Wextra -O2 -std=c99"
+
+# Languages able
+ABLE_LANGUAGES=("c")
+# Files sh for import
+IMPORT_FILES_SH='path_files\.sh|message_logs\.sh|interpret_line\.sh|import\.sh|import_src\.sh'
+
+
+# Standard attribute for project
+STANDARD_OPTIONS=" -n -p -l -develop -sources -is_main -src_sh -import_sh -maintenance -src_s_dir -src_m_dir -src_o_dir"
+STANDARD_ARGUMENTS="-n=$PROJECT_NAME -p=$PROJECT_PATH -l=c -develop=$DEVELOP_DIR_NAME -sources=$SOURCES_DIR_NAME -is_main=$FILE_MAIN_DIR_NAME -src_sh=$SRC_SH -import_sh=$IMPORT_SH -maintenance=$REPAIR_DIR -src_s_dir=$SRC_SCRIPT_DIR -src_m_dir=$SRC_MODULE_DIR -src_o_dir=$SRC_OBJECT_DIR -o"
+: '
+-nm: 			name project
+-p: 			path for project
+-l:				language for project ( only c now...)
+
+-develop: 		rename the develop dir
+-sources: 		rename the sources dir
+-is_main: 		rename the main file dir
+
+-src_sh: 		rename the source sh dir
+-import_sh:		rename the source of import sh dir
+-maintenance: 	rename the maintenance sh dir
+
+-src_s_dir		rename the source .c file dir
+-src_m_dir 		reamne the source heads files dir
+-src_o_dir 		rename the source object from .c files dir 
+
+-o 				unable the output of init_project
+'
 check_able_language(){ # language
 	local language=$1
 	for i in ${ABLE_LANGUAGES[@]};do
@@ -36,69 +87,44 @@ mount_build_files(){ # languages
 	done
 }
 mount_maintenance(){
-	mv $(echo "$(find ./for_init_project -type d -name $REPAIR_DIR)/*.sh") $PATH_REPAIR
+	cp $(echo "$(find ./for_init_project -type d -name $REPAIR_DIR)/*.sh") $PATH_REPAIR
 }
 mount_sh_dir(){
 	cp $(find . -type f | grep -P '('$IMPORT_FILES_SH')$') $PATH_IMPORT_SH
 	mount_build_files
 	mount_maintenance 
 
-	STANDARD_VALUES[1]="#"
-	mksrc "${STANDARD_VALUES[@]}"
+	STANDARD_VALUES[1]="."
+	mksrc "$(printf "%s " ${STANDARD_VALUES[@]})" "$(printf "%s " ${STANDARD_OPTIONS[@]})"
 
 	mv sources.sh $PATH_SRC_SH
 	put_path_src "$PATH_SRC_SH" "$IMPORT_SH|$SRC_SH"
 }
-
-# Global variables
-PROJECT_NAME="my_project"
-PROJECT_PATH="."
-
-# Name directories for main dirs project
-DEVELOP_DIR_NAME="develop"
-SOURCES_DIR_NAME="sources"
-FILE_MAIN_DIR_NAME="is_main"
-# for sh aplications
-SRC_SH="for_bash"
-IMPORT_SH="import_shell"
-# for repair the project
-REPAIR_DIR="maintenance"
-
-# for programming
-SRC_SCRIPT_DIR="src_scripts"
-SRC_MODULE_DIR="src_modules"
-SRC_OBJECT_DIR="src_objects"
-# for C language
-C_BUILD_DIR="c_build"
-C_COMPILE="gcc"
-C_FLAGS="-Wall -Wextra -O2 -std=c99"
-
-# Languages able
-ABLE_LANGUAGES=("c")
-# Files sh for import
-IMPORT_FILES_SH='path_files\.sh|message_logs\.sh|interpret_line\.sh|import\.sh|import_src\.sh'
-# Standard attribute for project
-STANDARD_OPTIONS="-n=$PROJECT_NAME -p=$PROJECT_PATH -l=c -develop=$DEVELOP_DIR_NAME -sources=$SOURCES_DIR_NAME -is_main=$FILE_MAIN_DIR_NAME -src_sh=$SRC_SH -import_sh=$IMPORT_SH -maintenance=$REPAIR_DIR -src_s_dir=$SRC_SCRIPT_DIR -src_m_dir=$SRC_MODULE_DIR -src_o_dir=$SRC_OBJECT_DIR -o"
-STANDARD_VALUES=($(interpret_options "$STANDARD_OPTIONS"))
-: '
--n: 		name project
--p: 		path for project
--l:			language for project ( only c now...)
-
--develop: 	rename the develop dir
--sources: 	rename the sources dir
--is_main: 	rename the main file dir
-
--src_sh: 	rename the source sh dir
-
--src_c_dir	rename the source .c file dir
--src_h_dir 	reamne the source heads files dir
--src_o_dir 	rename the source object from .c files dir 
-'
-
 main_init_project(){
-	local user_options="$@"
-	STANDARD_VALUES=($(interpret_options "$STANDARD_OPTIONS" "$user_options"))
+	PATH_MAIN=${PROJECT_PATH}/${PROJECT_NAME}
+	PATH_DEVELOP=${PATH_MAIN}/${DEVELOP_DIR_NAME}
+	PATH_MAIN_FILE=${PATH_DEVELOP}/${FILE_MAIN_DIR_NAME}
+
+	PATH_SRC_SH=${PATH_DEVELOP}/${SRC_SH}
+	PATH_IMPORT_SH=${PATH_SRC_SH}/${IMPORT_SH}
+	PATH_REPAIR=${PATH_SRC_SH}/${REPAIR_DIR}
+	#
+	PATH_SOURCE=${PATH_MAIN}/${SOURCES_DIR_NAME}
+	PATH_SCRIPT=${PATH_SOURCE}/${SRC_SCRIPT_DIR}
+	PATH_MODULE=${PATH_SOURCE}/${SRC_MODULE_DIR}
+	PATH_OBJECT=${PATH_SOURCE}/${SRC_OBJECT_DIR}
+	#
+	order_to_build=($PATH_MAIN $PATH_DEVELOP $PATH_MAIN_FILE $PATH_SRC_SH $PATH_IMPORT_SH $PATH_REPAIR $PATH_SOURCE $PATH_SCRIPT $PATH_MODULE $PATH_OBJECT)
+	for i in ${order_to_build[@]};do
+		mkdir -p $i
+		echo -e "\e[34m$i\e[0m"
+	done
+	#
+	mount_sh_dir 
+}
+read_input(){ # user_input
+	local user_input="$@"
+	STANDARD_VALUES=($(interpret_options "$STANDARD_ARGUMENTS" "$user_input"))
 	#
 	PROJECT_NAME=${STANDARD_VALUES[0]}
 	PROJECT_PATH=${STANDARD_VALUES[1]}
@@ -117,28 +143,15 @@ main_init_project(){
 	SRC_OBJECT_DIR=${STANDARD_VALUES[11]}
 
 	OUTPUT=${STANDARD_VALUES[$((${#STANDARD_VALUES[@]}-1))]}
-	#
-	PATH_MAIN=${PROJECT_PATH}/${PROJECT_NAME}
-	PATH_DEVELOP=${PATH_MAIN}/${DEVELOP_DIR_NAME}
-	PATH_MAIN_FILE=${PATH_DEVELOP}/${FILE_MAIN_DIR_NAME}
-
-	PATH_SRC_SH=${PATH_DEVELOP}/${SRC_SH}
-	PATH_IMPORT_SH=${PATH_SRC_SH}/${IMPORT_SH}
-	PATH_REPAIR=${PATH_SRC_SH}/${REPAIR_DIR}
-	#
-	PATH_SOURCE=${PATH_MAIN}/${SOURCES_DIR_NAME}
-	PATH_SCRIPT=${PATH_SOURCE}/${SRC_SCRIPT_DIR}
-	PATH_MODULE=${PATH_SOURCE}/${SRC_MODULE_DIR}
-	PATH_OBJECT=${PATH_SOURCE}/${SRC_OBJECT_DIR}
-	#
-	order_to_build=($PATH_MAIN $PATH_DEVELOP $PATH_MAIN_FILE $PATH_SRC_SH $PATH_IMPORT_SH $PATH_REPAIR $PATH_SOURCE $PATH_SCRIPT $PATH_MODULE $PATH_OBJECT)
-	for i in ${order_to_build[@]};do
-		mkdir -p $i
-		if [[ $OUTPUT = 0 ]];then
-			echo -e "\e[34m$i\e[0m"
-		fi
-	done
-	#
-	mount_sh_dir 
 }
-main_init_project "$@"
+run(){
+	read_input "$@"
+	rm -r ${PROJECT_PATH}/${PROJECT_NAME}
+	if [[ $OUTPUT = 1 ]];then
+		main_init_project > /dev/null
+	else
+		main_init_project
+	fi
+}
+
+run "$@"
