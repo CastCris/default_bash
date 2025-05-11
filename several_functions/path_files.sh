03be88d7390bb4396b6bd6dc4313b94a3d94e8ec
Cristiano <cristianochincarosanches@gmail.com>
2025-05-11 00:28:19 -0300

    update_path_files--improve_relative_path_and_create_switch_functions--

--- several_functions/path_files.sh
+++ several_functions/path_files.sh
@@ -3,59 +3,124 @@ source $(find . -type f -name import.sh)
 import_file "interpret_line.sh"
 
 relative_path(){ # -init : The point from start | -end : The point from destiny | -dir_start : The path directory where is the point start | -dir_end : The path_direcoty where if the point end
-	local standard_values="-init=. -end=home -dir_start=/home/$USER -dir_end=home"
+	local standard_values="-init=. -end=home -dir_start=/home/$USER -dir_end=home -search"
 	local user_input="$@"
 	local values=($(interpret_options "$standard_values" "$user_input"))
 
 	local init=${values[0]}
 	local end=${values[1]}
-	if [[ $init = "." ]];then
-		local temp=$(pwd)
-		init=${temp##*/}
-	fi
-	if [[ $end = "." ]];then
-		local temp=$(pwd)
-		end=${temp##*/}
-	fi
-
 	local dir_init=${values[2]}
 	local dir_end=${values[3]}
+	local search=${values[4]}
 
-	local path_init="`dirname $(find $dir_init -name ${init##*/} 2>/dev/null)/`"
-	local path_end="`dirname $(find $dir_end -name ${end##*/} 2>/dev/null)/`"
+	local path_init=""
+	local path_end=""
+	#
+	if [[ $init = "." ]];then
+		init=$(pwd)
+	fi
+	if [[ $end = "." ]];then
+		end=$(pwd)
+	fi
 
-	local path_smallest=${path_init}
-	local path_biggest=${path_end}
+	if [ -f $init ];then
+		init=${init%/*}
+	fi
+	if [ -f $end ];then
+		end=${end%/*}
+	fi
+	#
+	if [[ $search = 1 ]];then
+		local path_init="`dirname $(find $dir_init -name ${init##*/} 2>/dev/null)/`"
+		local path_end="`dirname $(find $dir_end -name ${end##*/} 2>/dev/null)/`"
+	else
+		switch_path "-path=$init"
+		path_init="$(pwd)"
+		resume_path
+		#
+		switch_path "-path=$end"
+		path_end="$(pwd)"
+		resume_path
+	fi
+	# echo "$path_init | $path_end"
+
+	local path_smallest=${path_init}/
+	local path_biggest=${path_end}/
+	local change=0
 
 	if [[ ${#path_smallest} -gt ${#path_biggest} ]];then
 		path_smallest=$path_end
 		path_biggest=$path_init
+		change=1
 	fi
 
+	local dir_commum=""
 	local path_commum=""
 	while [[ ${#path_smallest} -gt 0 ]];do
 		if [[ ${path_smallest%%/*} != ${path_biggest%%/*} ]];then
 			break
 		fi
-		path_commum=${path_smallest%%/*}
+		dir_commum=${path_smallest%%/*}
+		path_commum="$path_commum/$dir_commum"
+		#
 		path_smallest=${path_smallest#*/}
 		path_biggest=${path_biggest#*/}
-	done
 
-	local copy_path_init=${path_init%%/}
-	local relative_path=""
-	while [[ ${copy_path_init##*/} != $path_commum ]];do
-		relative_path="$relative_path/.."
-		copy_path_init=${copy_path_init%/*}
 	done
+	path_commum=${path_commum:1:${#path_commum}}
+
+	local path_back_sml="."
+	local path_head_sml=""
+	for i in $(echo ${path_smallest} | tr "/" "\n");do
+		path_back_sml="$path_back_sml/.."
+		path_head_sml="$path_head_sml/$i"
+	done
+	path_back_sml=${path_back_sml#*/}
+	path_head_sml=${path_head_sml%/*}
+	path_head_sml=${path_head_sml#/}
 	#
-	local copy_path_end=${path_end%%/}
-	local path_end_from_init=""
-	while [[ ${copy_path_end##*/} != $path_commum ]];do
-		path_end_from_init="${copy_path_end##*/}/$path_end_from_init"
-		copy_path_end=${copy_path_end%/*}
+	local path_back_big="."
+	local path_head_big=""
+	for i in $(echo ${path_biggest} | tr "/" "\n");do
+		path_back_big="$path_back_big/.."
+		path_head_big="$path_head_big/$i"
 	done
-	relative_path="$relative_path/$path_end_from_init"
+	path_back_big=${path_back_big#*/}
+	path_head_big=${path_head_big%/*}
+	path_head_big=${path_head_big#/}
+	
 
-	echo ".$relative_path"
+	local relative_path=""
+	if [[ $change = 0 ]];then
+		relative_path="$path_back_sml/$path_head_big"
+	else
+		relative_path="$path_back_big/$path_head_sml"
+	fi
+	#
+	if [[ ${relative_path:$((${#relative_path}-1)):1} = "/" ]];then
+		relative_path=${relative_path%%/}
+	fi
+
+	if [[ ${path_end##*/} != ${dir_commum} ]] || [[ $path_end != $path_commum ]];then
+		relative_path="$relative_path/${path_end##*/}"
+	fi
+	echo "$relative_path"
+}
+switch_path(){ # -path : The relative path for wish directory
+	local standard_values="-path=."
+	local user_input="$@"
+	local values=($(interpret_options "$standard_values" "$user_input"))
+
+	if [ -z $CURR_DIR ];then
+		CURR_DIR=$(pwd)
+	fi
+	local new_path=${values[0]}
+	cd $new_path
+}
+resume_path(){
+	if [ -z $CURR_DIR ];then
+		return
+	fi
+	cd $CURR_DIR
+	CURR_DIR=""
 }
